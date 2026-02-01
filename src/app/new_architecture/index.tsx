417de7d558f49fa2d774136958236ebfca797fc6
Asgeir <asgeira@unimicro.no>
2026-02-01 13:20:27 +0100

    WIP :: Native module example

--- src/app/new_architecture/index.tsx
+++ src/app/new_architecture/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import NativeBattery from "../../../specs/NativeBattery";
 
 export default function Index() {
+  const [batteryLevel, setBatteryLevel] = useState<number | null>(null);
+
+  useEffect(() => {
+    const level = NativeBattery.getBatteryLevel();
+    setBatteryLevel(level ?? null);
+  }, []);
 
   return (
     <View
@@ -12,6 +20,9 @@ export default function Index() {
       }}
     >
       <Text style={{ fontSize: 24, fontWeight: "bold" }}>New Architecture</Text>
+      <Text style={{ fontSize: 18 }}>
+        Battery Level: {batteryLevel !== null ? `${batteryLevel.toFixed(0)}%` : "Loading..."}
+      </Text>
     </View>
   );
 }
