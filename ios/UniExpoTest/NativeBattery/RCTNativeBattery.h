e23537412d62403b4f1a99df2a4172daee7c5472
Asgeir <asgeira@unimicro.no>
2026-02-01 13:31:20 +0100

    WIP :: Track native module files

---
+++ ios/UniExpoTest/NativeBattery/RCTNativeBattery.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <NativeBatterySpec/NativeBatterySpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeBattery : NSObject <NativeBatterySpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
